3a160b14bf7a4483bb513bb69560ca14d240f7e7
ZoruaStar64 <sanderb119@gmail.com>
2025-02-07 21:59:35 +0100

    Fixed removing domains from selection
    The problem was me forgetting to remove placeholder values given to the remove buttons.

--- HandleDomains.js
+++ HandleDomains.js
@@ -7,8 +7,8 @@ function Display_Domains() {
     for (let i = 0; i < domains.length; i++) {
         selectedDomainsHTML += "<p>" +
             domains[i].name + "." + domains[i].extension +
-            "  <form name='delete-domein"+ i +"'><input type='hidden' id='domainName"+ i +"' name='domainName"+ i +"' value='sb-dev'>" +
-            "<input type='hidden' id='domainExtension"+ i +"' name='domainExtension"+ i +"' value='nl'>" +
+            "  <form name='delete-domein"+ i +"'><input type='hidden' id='domainName"+ i +"' name='domainName"+ i +"' value='"+ domains[i].name +"'>" +
+            "<input type='hidden' id='domainExtension"+ i +"' name='domainExtension"+ i +"' value='"+ domains[i].extension +"'>" +
             "<button type='button' onclick='Remove_Domain(\""+ i +"\")'>Verwijder domein van selectie</button></form> <br>";
     }
     selectedDomainsHTML += "</p>";
@@ -19,25 +19,18 @@ function Add_Domain() {
     let name = document.getElementById("domein-naam").value;
     let extension = document.getElementById("domein-extensie").value;
     domains.push({"name": name, "extension": extension});
-    console.log(domains);
     Display_Domains();
 }
 
-//Make id search more dynamic by changing the ids name and not just the number.
 function Remove_Domain(id) {
     let domainName = document.getElementById("domainName" + parseInt(id)).value;
     let domainExtension = document.getElementById("domainExtension" + parseInt(id)).value;
     var domainToRemove = 0;
     for (let i = 0; i < domains.length; i++) {
-        console.log("domain " + i + ": " + domains[i].name + domains[i].extension);
         if (domains[i].name === domainName && domains[i].extension === domainExtension) {
-            console.log(domains[i].name + domains[i].extension + " | " + domainName + domainExtension);
             domainToRemove = i;
         }
     }
-    console.log("removing name: " + domainName)
-    console.log("removing extension: " + domainExtension + parseInt(id));
-    console.log("index removed: " + domainToRemove);
     domains.splice(domainToRemove,1);
     Display_Domains();
 }
