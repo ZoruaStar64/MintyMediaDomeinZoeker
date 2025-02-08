6b00fc1c49cbd3fdef5c55c6fa2dda551b72d4f5
ZoruaStar64 <sanderb119@gmail.com>
2025-02-08 21:12:28 +0100

    Preparations for API integration
    Renamed a couple of tag names/ids so that they're consistent among the others. Added a maximum domain selection limit. added the search domain button, which also gets (de)activated depending on if a domain is selected or not.

--- HandleDomains.js
+++ HandleDomains.js
@@ -1,30 +1,51 @@
 let domains = [];
 let validExtensions = ["com", "org", "net", "nl", "eu"];
+let maxDomains = 10;
+
+Check_Selected_Domains();
 
 function Display_Domains() {
-    let container = document.getElementById("domein_container");
+    let container = document.getElementById("domein-container");
     let selectedDomainsHTML = "";
     for (let i = 0; i < domains.length; i++) {
         selectedDomainsHTML += "<p>" +
             domains[i].name + "." + domains[i].extension +
-            "  <form name='delete-domein"+ i +"'><input type='hidden' id='domainName"+ i +"' name='domainName"+ i +"' value='"+ domains[i].name +"'>" +
-            "<input type='hidden' id='domainExtension"+ i +"' name='domainExtension"+ i +"' value='"+ domains[i].extension +"'>" +
+            "  <form name='delete-domein"+ i +"'><input type='hidden' id='domain-name"+ i +"' name='domain-name"+ i +"' value='"+ domains[i].name +"'>" +
+            "<input type='hidden' id='domain-extension"+ i +"' name='domain-extension"+ i +"' value='"+ domains[i].extension +"'>" +
             "<button type='button' onclick='Remove_Domain(\""+ i +"\")'>Verwijder domein van selectie</button></form> <br>";
     }
     selectedDomainsHTML += "</p>";
     container.innerHTML = selectedDomainsHTML;
+    Set_Select_Form_Value();
+    Check_Selected_Domains();
+}
+
+function Set_Select_Form_Value() {
+    let formValues = document.getElementById("geselecteerde-domeinen");
+    formValues.value = JSON.stringify(domains);
+}
+
+function Check_Selected_Domains() {
+    let searchButton = document.getElementById("domein-zoekknop");
+    if (domains.length === 0) {
+        searchButton.disabled = true;
+    } else if (domains.length > 0) {
+        searchButton.disabled = false;
+    }
 }
 
 function Add_Domain() {
-    let name = document.getElementById("domein-naam").value;
-    let extension = document.getElementById("domein-extensie").value;
-    domains.push({"name": name, "extension": extension});
+    if (domains.length !== maxDomains && domains.length < maxDomains) {
+        let name = document.getElementById("domein-naam").value;
+        let extension = document.getElementById("domein-extensie").value;
+        domains.push({"name": name, "extension": extension});
+    }
     Display_Domains();
 }
 
 function Remove_Domain(id) {
-    let domainName = document.getElementById("domainName" + parseInt(id)).value;
-    let domainExtension = document.getElementById("domainExtension" + parseInt(id)).value;
+    let domainName = document.getElementById("domain-name" + parseInt(id)).value;
+    let domainExtension = document.getElementById("domain-extension" + parseInt(id)).value;
     var domainToRemove = 0;
     for (let i = 0; i < domains.length; i++) {
         if (domains[i].name === domainName && domains[i].extension === domainExtension) {
--- index.php
+++ index.php
@@ -12,13 +12,17 @@
                 Typ hier uw gewenste domein naam op.
                 <input type="text" id="domein-naam" name="domein-naam" required>
             </label>
-            <label for="domein_extensie">
+            <label for="domein-extensie">
                 Typ hier uw gewenste domein extensie op (com, nl, enzovoort).
                 <input type="text" id="domein-extensie" name="domein-extensie" required>
             </label>
             <button type="button" onclick="Add_Domain()">Voeg domein toe aan selectie</button>
         </form>
-    <div id="domein_container"></div>
+        <div id="domein-container"></div>
+        <form name="domein-zoek-formulier" action="SearchDomains.php" method="post" enctype="application/x-www-form-urlencoded">
+            <input type="hidden" name="geselecteerde-domeinen" id="geselecteerde-domeinen" value="hi">
+            <input type="submit" name="domein-zoekknop" id="domein-zoekknop" value="Bekijk beschikbaarheid">
+        </form>
     </body>
 <script src="HandleDomains.js" type="application/javascript"></script>
 </html>
